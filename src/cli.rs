b13820ea41c3dddfd0e69a895b086745fbf05abd
Christian Döring <chipfunk@gmx.de>
2026-07-26 22:35:37 +0200

    Initial support for configuration via env-variables

--- src/cli.rs
+++ src/cli.rs
@@ -15,38 +15,42 @@ pub enum Commands {
         keyfile: Option<String>,
     },
     Start {
-        #[clap(long)]
+        #[clap(long, env = "VPNR_IP_ADDRESS")]
         ip_addr: Option<IpAddr>,
         /// The network-interface to use
         #[clap(long)]
         interface_name: Option<String>,
-        #[clap(long)]
+        #[clap(long, env = "VPNR_LISTEN_ADDR")]
         listen_addr: Option<IpAddr>,
-        #[clap(long)]
+        #[clap(long, env = "VPNR_LISTEN_PORT")]
         listen_port: Option<u16>,
         /// The file containing the private-key
-        #[clap(long, default_value = "vpnr_ed25519")]
+        #[clap(
+            long,
+            default_value = "vpnr_ed25519",
+            env = "VPNR_PRIVATE_KEYFILE_PATH"
+        )]
         keyfile: Option<String>,
         /// Enable libp2p::identify
-        #[clap(long)]
+        #[clap(long, env = "VPNR_ENABLE_IDENTIFY")]
         enable_identify: Option<bool>,
         /// Enable libp2p::DHT
-        #[clap(long)]
+        #[clap(long, env = "VPNR_ENABLE_DHT")]
         enable_dht: Option<bool>,
         /// Enable libp2p::mDNS
-        #[clap(long)]
+        #[clap(long, env = "VPNR_ENABLE_MDNS")]
         enable_mdns: Option<bool>,
         /// Enable libp2p::UPnP
-        #[clap(long)]
+        #[clap(long, env = "VPNR_ENABLE_UPNP")]
         enable_upnp: Option<bool>,
         /// Enable libp2p::relaying
-        #[clap(long)]
+        #[clap(long, env = "VPNR_ENABLE_RELAY")]
         enable_relay: Option<bool>,
         /// Enable libp2p::dcutr
-        #[clap(long)]
+        #[clap(long, env = "VPNR_ENABLE_DCUTR")]
         enable_dcutr: Option<bool>,
         /// Enable libp2p::autonat
-        #[clap(long)]
+        #[clap(long, env = "VPNR_ENABLE_AUTONAT")]
         enable_autonat: Option<bool>,
     },
 }
